d05fc4c5a3b2ebd8caff746d01ba11cd5feb886a
Brandon <bffalk@oakland.edu>
2019-02-08 16:11:15 -0500

    main function implemented

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -2,13 +2,14 @@ public class BeerSong {
 
     public static void main(String[] args) {
 
-
-
+    System.out.println("***************** Program OutPut *****************");
+    Ninety_Nine_Bottles_of_Beer();
+    System.out.println("*****************  End of OutPut *****************");
     }
 
     public static void Ninety_Nine_Bottles_of_Beer(){
 
-
+        
 
     }
 }
